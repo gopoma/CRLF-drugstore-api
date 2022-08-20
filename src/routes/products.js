f13f02b88b2761839502580fef29ae4a29b4f959
Gustavo <gordono@unsa.edu.pe>
2022-08-20 00:00:51 -0500

    Added Edit all cases product functionality

--- src/routes/products.js
+++ src/routes/products.js
@@ -14,7 +14,17 @@ function products(app) {
 
   router.post("/", async (req, res) => {
     const result = await productService.create(req.body);
-    return res.status(result.success ? 200 : 400).json(result);
+    return res.status(result.success ? 201 : 400).json(result);
+  });
+
+  router.patch("/:idProduct", async (req, res) => {
+    const result = await productService.edit(req.params.idProduct, req.body);
+    return res.status(result.success ? 202 : 400).json(result);
+  });
+
+  router.put("/:idProduct", async (req, res) => {
+    const result = await productService.update(req.params.idProduct, req.body);
+    return res.status(result.success ? 202 : 400).json(result);
   });
 
   router.delete("/:idProduct", async (req, res) => {
--- src/services/products.js
+++ src/services/products.js
@@ -22,6 +22,43 @@ class ProductService {
     }
   }
 
+  async edit(idProduct, data) {
+    try {
+      const product = await ProductModel.findOneAndUpdate(idProduct, data, {new:true});
+      return {
+        success: true,
+        product
+      };
+    } catch(error) {
+      return {
+        success: false,
+        errors: dbError(error)
+      };
+    }
+  }
+
+  async update(idProduct, data) {
+    try {
+      if(!data.name || !data.price || !data.description) {
+        return {
+          success: false,
+          errors: ["Fill all the necessary fields"]
+        };
+      }
+
+      const product = await ProductModel.findOneAndUpdate(idProduct, data, {new:true});
+      return {
+        success: true,
+        product
+      }
+    } catch(error) {
+      return {
+        success: false,
+        errors: dbError(error)
+      };
+    }
+  }
+
   async delete(idProduct) {
     try {
       const product = await ProductModel.findByIdAndDelete(idProduct);
